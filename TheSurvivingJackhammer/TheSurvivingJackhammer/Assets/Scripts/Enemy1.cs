902a8c4c52403a655acc1fef0b3860f4344bde0b
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2019-02-27 09:34:07 -0300

    Final version with Google Play Games

---
+++ TheSurvivingJackhammer/TheSurvivingJackhammer/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour {
+
+	GameObject mainChar;
+    DifficultyManager difficultyManager;
+	float Speed;
+
+	void Start(){
+		mainChar = GameObject.Find("MainCharSpawn");
+        difficultyManager = GameObject.Find("DifficultyManager").GetComponent<DifficultyManager>();
+        Speed = difficultyManager.initialEnemy1Speed;
+	}
+
+	/// <summary>
+    /// Move o inimigo em direção do personagem principal
+    /// </summary>
+    /// <param name="speed"></param>
+	void move (float speed) {
+		Vector3 movDir = (mainChar.transform.position - transform.position).normalized;
+		transform.position += movDir* Time.deltaTime * speed;
+	}
+	
+	void FixedUpdate () {
+		move(Speed);
+	}
+}
